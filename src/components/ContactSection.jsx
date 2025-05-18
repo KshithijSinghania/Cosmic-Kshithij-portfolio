df64a27d082d32908a746063b640e9a87201be32
KshithijSinghania <142089913+KshithijSinghania@users.noreply.github.com>
2025-05-18 11:00:36 +0530

    Update ContactSection.jsx

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -57,7 +57,7 @@ export const ContactSection = () => {
                 <div>
                   <h4 className="font-medium"> Email</h4>
                   <a
-                    href="mailto:kshithijsinghaniao@gmail.com"
+                    href="mailto:kshithijsinghania@gmail.com"
                     className="text-muted-foreground hover:text-primary transition-colors"
                   >
                     kshithijsinghaniao@gmail.com
